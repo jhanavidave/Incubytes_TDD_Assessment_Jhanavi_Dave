0d01328dda099bbb0c9a794ac837fda27413f4d4
Jhanavi <jhanavi.dave97@gmail.com>
2025-01-03 17:06:43 +0530

    adding arrayed numbers

---
+++ AddString.java
@@ -0,0 +1,12 @@
+// JhanaviDave
+public class AddString {
+
+    public static void main(String[] args) {
+        int num = 0;
+        int[] numStr = {1, 2, 3};
+        for (int i = 0; i < numStr.length; i++) {
+            num = num + numStr[i];
+        }
+        System.out.println(num);
+    }
+}
