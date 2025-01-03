061db313064afa7913eb370218357fde2cf69cfe
Jhanavi <jhanavi.dave97@gmail.com>
2025-01-03 17:09:33 +0530

    splitting string to int array

--- AddString.java
+++ AddString.java
@@ -1,9 +1,12 @@
-// JhanaviDave
+
+import java.util.Arrays;
+
 public class AddString {
 
     public static void main(String[] args) {
+        String numString = "1,2;3";
         int num = 0;
-        int[] numStr = {1, 2, 3};
+        int[] numStr = Arrays.stream(numString.split("[,; ]+")).mapToInt(Integer::parseInt).toArray();
         for (int i = 0; i < numStr.length; i++) {
             num = num + numStr[i];
         }
